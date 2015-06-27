504b9959b3eb3943a6fb3dcf25ed2f2a66578292
sadikovi <ivan.sadikov@lincolnuni.ac.nz>
2015-06-27 12:42:24 +1200

    some testing experiments

--- bar/src/test/scala/BarSpec.scala
+++ bar/src/test/scala/BarSpec.scala
@@ -1,9 +1,32 @@
 import collection.mutable.Stack
 import org.scalatest._
+import scala.util.Random
+
 
 class BarSpec extends UnitTestSpec {
-  "A Bar" should "execute test" in {
-		val a = Array(1, 2, 3)
-		a.length should be (3)
-  }
+    val expression = false
+
+    def sample = Random.nextInt(100)
+
+    "A Bar" should "execute test" in {
+        val a = Array(1, 2, 3)
+        a.length should be (3)
+    }
+
+    ignore should "ignore test" in {
+        val a = Array(1, 2, 3)
+        a.sum should be (6)
+    }
+
+    it should "generate number" in {
+        sample should be >= (0)
+    }
+
+    if (expression) {
+        it should "do something" in {
+            true should be (true)
+        }
+    } else {
+        ignore should "support only Linux" in {}
+    }
 }
